100a49ab6e92f7dcd9772584712143b6419d0a0d
adriansbruno <114193634+adriansbruno@users.noreply.github.com>
2023-05-01 13:03:46 -0400

    added component code, updated files and other code

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import './stylesheet/app.css';
+import Router from "./router";
+
+function App() {
+  return (
+    <div className="App">
+      <router />
+    </div>
+  );
+}
+
+export  default App;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+    <div class="container">
+    <ul>
+      <li><a href="https://github.com/adriansbruno" target="_blank"><i class="fab fa-github"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/username/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+      <li><a href="https://twitter.com/username" target="_blank"><i class="fab fa-twitter"></i></a></li>
+    </ul>
+  </div>
+    )
+}
